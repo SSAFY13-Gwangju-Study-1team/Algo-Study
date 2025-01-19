188a10d3e5e05589041bdc3956f9f046bb7cdb70
seojjin <002seojin@gmail.com>
2025-01-19 17:39:07 +0900

    박서진 1주차 17413 단어뒤빕기2

---
+++ 박서진/1week/Main_17413_단어뒤집기2.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+public class Main_17413_단어뒤집기2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String sentence = br.readLine();
+        char[] s_arr = sentence.toCharArray();
+        Stack<Character> st = new Stack<>();
+        boolean tag = false;
+        for (char ch:s_arr){
+            if(ch == '<'){
+                while(!st.isEmpty()){
+                    System.out.print(st.pop());
+                }
+                tag = true;
+                System.out.print(ch);
+            }else if(ch == '>'){
+                tag = false;
+                System.out.print(ch);
+            }else if(tag){
+                System.out.print(ch);
+            }else{
+                if (ch==' '){
+                    while(!st.isEmpty()){
+                        System.out.print(st.pop());
+                    }
+                    System.out.print(' ');
+                }else{
+                    st.push(ch);
+                }
+            }
+        }
+        while(!st.isEmpty()){
+            System.out.print(st.pop());
+        }
+    }
+}
