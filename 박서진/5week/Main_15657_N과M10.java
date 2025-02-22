0b3ba9996ac403ef1d88d179a3c800c57e067df8
seojjin <002seojin@gmail.com>
2025-02-23 08:30:08 +0900

    n과m10

---
+++ 박서진/5week/Main_15657_N과M10.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+
+public class Main_15657_N과M10 {
+    static int n, m;
+    static int[] nums;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = parseInt(st.nextToken());
+        m = parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        nums = new int[n];
+
+        for(int i=0;i<n;i++){
+            nums[i] = parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+        backtrack(0,  0, new int[m]);
+        System.out.println(sb);
+    }
+    public static void backtrack(int depth, int start, int[] selected){
+        if(depth==m){
+            for(int i:selected){
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int prevNum=0;
+
+        for(int i=start;i<n;i++){
+            if(prevNum==nums[i]) continue;
+            selected[depth] = nums[i]; //백트래킹 호출 이전에 해줘야 함 반복문을 돌면서 숫자를 선택할 때 즉시 갱신해야 해!
+            prevNum = nums[i];
+            backtrack(depth+1, i+1, selected);
+        }
+
+
+
+    }
+}
---
+++ 박서진/5week/Main_15657_N과M10_ver2.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+
+public class Main_15657_N과M10_ver2 {
+    static int n, m;
+    static int[] nums;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = parseInt(st.nextToken());
+        m = parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        nums = new int[n];
+
+        for(int i=0;i<n;i++){
+            nums[i] = parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+        backtrack(0,  0, new int[m], -1);
+        System.out.println(sb);
+    }
+
+    public static void backtrack(int depth, int target, int[] selected, int prevNum){
+        if(depth==m){
+            for(int i:selected){
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        if (target>=n) return;
+        if(prevNum==nums[target]) return;
+        selected[depth] = nums[target];
+        // 현재 인덱스를 고를 때 
+        backtrack(depth+1, target+1, selected, -1);
+        // 현재 인덱스를 고르지 않을 때
+        backtrack(depth, target+1, selected, nums[target]);
+    }
+}
