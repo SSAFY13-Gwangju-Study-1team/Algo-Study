2351a67cae3018d7ac1bc2e8c9b9d0e8e754da8e
SJY <jun000405@naver.com>
2025-02-25 20:47:29 +0900

    feat: junyoung 0225 (17281) 시뮬레이션, 순열

--- 송준영/Main_17281_야구_송준영.java
+++ 송준영/Main_17281_야구_송준영.java
@@ -1,192 +1,132 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import static java.lang.Integer.parseInt;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
+import static java.lang.Integer.parseInt;
 
+/**
+ * Main_17281_야구_송준영
+ * 난이도 7/10
+ * 순열
+ * 468ms 15,000KB
+ * 
+ * 순열을 이용하여 1번 선수부터 9번 선수까지 타순을 정하고
+ * 각 타순에 대해 게임 시뮬레이션을 수행하여 최대 점수를 구함
+ * 나는 각 이닝 마다 최적의 순서를 찾아서 하는 줄 알았다...
+ * 이래서 문제를 잘 읽어야 한다.
+ * 1번째 선수는 4번으로 고정되어 있어야 하므로 이것을 구현하는게 중요 할 것이다.
+ */
 public class Main_17281_야구_송준영 {
-    // 빠른 입출력을 위한 선언
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
 
-    static int N;
-    static List<List<Integer>> players;
-    static List<Set<List<Integer>>> permus;
-    static int maxScore = Integer.MIN_VALUE;
+    static int N;                           // 이닝 수
+    static int[][] players;                 // game[i][j] : i번째 이닝에서 j번 선수의 결과
+    static int maxScore = 0;                // 최대 점수
+    static int[] order = new int[9];        // 타순 (배열의 값은 선수 번호)
+    static boolean[] used = new boolean[9]; // 순열 생성시 사용 여부 (player 0은 고정)
 
     public static void main(String[] args) throws Exception {
+        // 입력 처리
         N = parseInt(br.readLine());
+        players = new int[N][9];
 
-        players = new ArrayList<>();
-        permus = new ArrayList<>();
-
+        // 입력 받기
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            players.add(new LinkedList<>());
-            permus.add(new HashSet<>());
-
             for (int j = 0; j < 9; j++) {
-                int temp = parseInt(st.nextToken());
-                players.get(i).add(temp);
+                players[i][j] = parseInt(st.nextToken());
             }
         }
 
-        permutation();
+        // player 0을 4번 타자로 고정 (인덱스 3에 고정)
+        order[3] = 0;
+        used[0] = true;
 
-        recursive(0, 0, 0);
+        // 순열 생성
+        permute(0);
 
+        // 결과 출력
         System.out.println(maxScore);
-
-        // for (int i = 0; i < N; i++) {
-        //     for (List<Integer> e : permus.get(i)) {
-        //         for (int in : e) {
-        //             System.out.print(in + " ");
-        //         }
-        //         System.out.println();
-        //     }
-        //     System.out.println();
-        // }
-
     }
 
-    public static void recursive(int depth, int score, int order) {
-        if (depth == N) {
+    /**
+     * 순열 생성
+     * @param depth 순열의 깊이
+     */
+    static void permute(int depth) {
+        if (depth == 9) { // 타순 완성 → 게임 시뮬레이션
+            int score = game();
             maxScore = Math.max(maxScore, score);
             return;
         }
-
-        for (List<Integer> p : permus.get(depth)) {
-            int[] order_score = play(order, p);
-            recursive(depth + 1, score + order_score[1], order_score[0]);
+        if (depth == 3) { // 4번 타자는 고정되어 있으므로 넘어감
+            permute(depth + 1);
+            return;
         }
-    }
-    public static int[] play(int order, List<Integer> players) {
 
-        boolean[] plate = new boolean[4];
-        int outCnt = 0;
-        int score = 0;
-
-        while (outCnt < 3) {
-            switch (players.get(order)) {
-                case 0:
-                    outCnt++;
-                    break;
-                case 1:
-                    for (int i = 3; i >= 1; i--) {
-                        if (plate[i] == true) {
-                            if (i + 1 > 3) {
-                                score++;
-                                plate[i] = false;
-                            } else {
-                                plate[i] = false;
-                                plate[i + 1] = true;
-                            }
-                        }
-                    }
-                    plate[1] = true;
-                    break;
-                case 2:
-                    for (int i = 3; i >= 1; i--) {
-                        if (plate[i] == true) {
-                            if (i + 2 > 3) {
-                                score++;
-                                plate[i] = false;
-                            } else {
-                                plate[i] = false;
-                                plate[i + 2] = true;
-                            }
-                        }
-                    }
-                    plate[2] = true;
-                    break;
-                case 3:
-                    for (int i = 3; i >= 1; i--) {
-                        if (plate[i] == true) {
-                            if (i + 3 > 3) {
-                                score++;
-                                plate[i] = false;
-                            } else {
-                                plate[i] = false;
-                                plate[i + 3] = true;
-                            }
-                        }
-                    }
-                    plate[3] = true;
-                    break;
-                case 4:
-                    score++;
-                    for (int i = 3; i >= 1; i--) {
-                        if (plate[i] == true) {
-                            score++;
-                            plate[i] = false;
-                        }
-                    }
-                    break;
+        // 각 선수 순서 순열 생성
+        for (int player = 1; player < 9; player++) {
+            if (!used[player]) {
+                used[player] = true;
+                order[depth] = player;
+                permute(depth + 1);
+                used[player] = false;
             }
-
-            order++;
-            order %= 9;
-        }
-
-        return new int[] { order, score };
-
-    }
-
-    public static void permutation() {
-        for (int i = 0; i < N; i++) {
-            int first = players.get(i).get(0);
-
-            players.get(i).remove(0);
-            Collections.sort(players.get(i));
-
-            do {
-                List<Integer> temp = new LinkedList<>(players.get(i));
-                temp.add(3, first);
-                permus.get(i).add(temp);
-                // System.out.println("되나?" + i);
-            } while (NP(i));
         }
     }
 
-    public static boolean NP(int index) {
-        int i = 7;
+    /**
+     * 게임 시뮬레이션
+     * @return  게임 결과 점수
+     */
+    static int game() {
+        int score = 0;
+        int playerOrder = 0; // 다음 타자 인덱스 (0~8)
 
-        while (i > 0 && players.get(index).get(i - 1) >= players.get(index).get(i)) {
-            --i;
-            // System.out.println(players.get(index).get(i - 1));
-            // System.out.println(players.get(index).get(i));
+        // 각 이닝에 대해 게임 시뮬레이션
+        for (int inning = 0; inning < N; inning++) {
+            int outs = 0;
+            int first = 0, second = 0, third = 0;
+
+            // 3아웃까지 반복
+            while (outs < 3) {
+                int player = order[playerOrder];
+                int result = players[inning][player];
+
+                switch (result) {
+                    case 0: // 아웃
+                        outs++;
+                        break;
+                    case 1: // 안타
+                        score += third;
+                        third = second;
+                        second = first;
+                        first = 1;
+                        break;
+                    case 2: // 2루타
+                        score += third + second;
+                        third = first;
+                        second = 1;
+                        first = 0;
+                        break;
+                    case 3: // 3루타
+                        score += third + second + first;
+                        third = 1;
+                        second = 0;
+                        first = 0;
+                        break;
+                    case 4: // 홈런
+                        score += third + second + first + 1;
+                        third = 0;
+                        second = 0;
+                        first = 0;
+                        break;
+                    }
+                // 다음 타자
+                playerOrder++;
+                playerOrder %= 9;
+            }   
         }
-            
-
-        if (i == 0) {
-            return false;
-        }
-
-        int j = 7;
-
-        while (players.get(index).get(i - 1) >= players.get(index).get(j))
-            --j;
-
-        swap(i - 1, j, index);
-
-        int k = 7;
-
-        while (i < k) {
-            swap(i++, k--, index);
-        }
-
-        return true;
-    }
-    
-    public static void swap(int x, int y, int index) {
-        int temp = players.get(index).get(x);
-        players.get(index).set(x, players.get(index).get(y));
-        players.get(index).set(y, temp);
+        return score;
     }
 }
