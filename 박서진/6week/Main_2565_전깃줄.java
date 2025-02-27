fab225a7e0065ff8a447edb81655b8c263b4777c
seojjin <002seojin@gmail.com>
2025-02-28 01:08:59 +0900

    2565 전깃줄

---
+++ 박서진/6week/Main_2565_전깃줄.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+import static java.lang.Integer.parseInt;
+public class Main_2565_전깃줄 {
+    public static void main(String[] args) throws Exception{
+
+
+    }
+}
